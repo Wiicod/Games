663580c716616b556a60a39b7fc488b4c0f7a6de
Zephyr08 <enanda52@gmail.com>
2016-03-16 17:00:36 +0100

    Mise au point des boutons retour

--- speedy-click/js/controllers.js
+++ speedy-click/js/controllers.js
@@ -16,11 +16,7 @@ angular.module('sc.controllers', [])
     }])
 
     .controller('HomeCtrl', ['$scope','$rootScope',function($scope,$rootScope) {
-
-    }])
-
-    .controller('MenuCtrl', ['$scope','$rootScope',function($scope,$rootScope){
-        
+        $('.modal-backdrop').css({'display':'none'});
     }])
 
     .controller('HelpCtrl', ['$scope',function($scope,NavbarFactory) {
@@ -31,13 +27,62 @@ angular.module('sc.controllers', [])
         
     }])
 
-    .controller('PlayCtrl',  ['$scope',function($scope,NavbarFactory) {
+    .controller('PlayCtrl',  ['$scope','$interval',function($scope,$interval) {
+        $('.modal-backdrop').css({'display':'none'});
+
+        $scope.time=10;
+        var chrono;
+        var state=true;
+
         $scope.hit=0;
         $scope.score=$scope.hit+" hit";
         $scope.Click=function(){
-            $scope.hit+=10;
-            $scope.score=$scope.hit+" hits";
+            if(state){
+                $scope.StartChrono();
+                $scope.hit+=10;
+                $scope.score=$scope.hit+" hits";
+            }
         }
+
+        $('#pauseModal').click(function(){
+            console.log("q");
+        })
+
+        // Gestion du chrono
+        $scope.StartChrono = function() {
+            // Don't start a new fight if we are already fighting
+            if ( angular.isDefined(chrono) ) return;
+
+            chrono = $interval(function() {
+                if($scope.time>0){
+                    $scope.time--;
+                    if($scope.time==0){
+                        state=false;
+                    }
+                }
+                else{
+                    state=false;
+                    $scope.StopChrono();
+                }
+
+            }, 1000);
+        };
+
+        $scope.StopChrono=function(){
+            $interval.cancel(chrono);
+            chrono=undefined;
+        }
+
+        $scope.Restart=function(){
+            $('.modal-backdrop').css({'display':'none'});
+            $('#pauseModal').css({'display':'none'});
+            $scope.hit=0
+            $scope.score="0 hit";
+            $scope.time=10;
+            $scope.StartChrono();
+        }
+
+        $scope.StartChrono();
     }])
 
     .controller('OptionCtrl',  ['$scope',function($scope,NavbarFactory) {
